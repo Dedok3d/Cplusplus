e07d57d635260a974587ade8f8c3b8aabe5bd54f
Sergey <Sersh_96@mail.ru>
2016-03-23 01:28:38 +0600

    Create OS_lub5.cpp

---
+++ OS_lub5.cpp
@@ -0,0 +1,76 @@
+#include <string.h>;
+#include <fcntl.h>;
+#include <vector>;
+#include <unistd.h>
+#include <iostream>
+class Str{
+    int startPos;
+    int numLet;
+    public:
+    Str(){
+        startPos = 0;
+        numLet = 0;
+    }
+    Str(int a, int b){
+        startPos = a;
+        numLet = b;
+    }
+
+    void SetPos(int a){
+        startPos = a;
+    }
+
+    void SetNum(int b){
+        numLet = b;
+    }
+
+    int GetPos(){
+        return startPos;
+    }
+
+    int GetNum(){
+        return numLet;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+
+    int n = 0;
+    int s = 0;
+    Str tmp();
+    std::vector<Str> arr;
+    int input_file = 0;
+    input_file = open("in.txt", O_RDONLY);
+    if(input_file<0){
+        std::cout << "We are sorry, but we can't fund this file" << std::endl;
+        return 0;
+    }
+    char buff[1024];
+    //char buff1[1024];
+    memset(buff, 0, 1024);
+    //memset(buff1, 0, 1024);
+   // lseek(input_file,7,0);
+    while(read(input_file, buff, 1)) {
+        std::cout << buff;
+        n++;
+        if(buff[0]=='\n') {
+            arr.insert(arr.end(),Str(s,(n-s-1)));
+            s = n;
+        }
+    }
+    memset(buff, 0, 1024);
+    std::cin >> n;
+    if(n<0 || n>arr.size()){
+        std::cout << "We are sorry, but this line doesn't exist" << std::endl;
+        return 0;
+    }
+    lseek(input_file,arr[n-1].GetPos(),0);
+    for(int i=0; i< arr[n-1].GetNum(); i++) {
+        read(input_file, buff, 1);
+        std::cout << buff;
+    }
+    std::cout << std::endl;
+    close(input_file);
+    return 0;
+}
