70600fc9ad1ff2fc14f807661ac3db0d26d4756c
Sergey <Sersh_96@mail.ru>
2016-04-29 11:01:31 +0600

    Create 25.cpp

---
+++ 25.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main()
+{
+  pid_t pid; /*уникальный внутри системы номер, который называется PID (аббревиатура от process identifier)*/
+  int rv;
+  switch(pid=fork()) {
+     case -1: /{*}при вызове fork() возникла ошибка{*}/
+        perror("fork"); /* error */
+        exit(1); /*exit*/
+     case 0: /{*}это код потомка{*}/
+        printf(" CHILD: This is the child process!\n");
+        execl("/bin/cat", "cat", "in.txt", (char *)NULL);
+     default: /{*}это код родительского процесса{*}/
+        printf("PARENT: This is the parent process!\n");
+        printf("PARENT: my PID -- %d\n", getpid());
+        printf("PARENT: My child PID %d\n",pid);
+        printf("PARENT: I wait? while my child don't call exit()...\n");
+        wait(&rv); /*Ожидание потомка*/
+        printf("PARENT: Exit!\n");
+  }
+  return 0;
+}
